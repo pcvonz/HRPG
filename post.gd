d6dde1066bb28e98f8392645ac5afe653fa8dcaf
Paul Von Zimmerman <pcvonz@gmail.com>
2015-05-09 12:41:32 -0700

    responsive tweaks

--- post.gd
+++ post.gd
@@ -1,26 +1,20 @@
 
-extends Node2D
+extends Panel
 
 # member variables here, example:
-# var a=2
-# var b="textvar"
+var children
 var task_id
 var url = "/api/v2/user/tasks/"
 
 func post_task():
 	url += task_id + "/up"
-#	get_parent().get_node("http").http_post(task_id, url)
-	print(get_parent())
-	get_parent().const_margin_top -= 37.5
-	get_parent().set_margin(MARGIN_TOP, get_parent().const_margin_top)
+
 	##Should make this grayed out instead.
 	get_parent().remove_child(self)
 	
 	
-func _process(delta):
-	get_parent().set_size(OS.get_window_size())
-	
 func _ready():
-	set_process(true)
+	pass
+
 
 
