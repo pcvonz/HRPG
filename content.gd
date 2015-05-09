a371d0cfaa9b5b412c1a96f8a2b28bb9197cd191
Paul Von Zimmerman <pcvonz@gmail.com>
2015-05-09 12:41:24 -0700

    responsive tweeks

--- content.gd
+++ content.gd
@@ -5,6 +5,7 @@ var button
 var button_number = 0
 var panel
 var label
+
 #change vector size with variable.x and variable.y
 var button_size_begin = Vector2()
 var button_size_end = Vector2()
@@ -19,54 +20,50 @@ func _init():
 	panel_pos.y = -60
 	panel_pos.x = 200
 	panel_size_end.x = 300
-	panel_size_end.y = 60
+	panel_size_end.y = 10
 
 func _on_button_pressed():
-	print("HELLO")
 	_create_widget()
 
 #useful canvas item stuff. get_parent, get_size, set_size.
 func _create_widget(text, id):
-	button_size_begin.x = 200
-	button_size_begin.y = 0
-	button_size_end.x = 200
-	button_size_end.y = 0
 	panel_pos.y = panel_pos.y + 40
 	
-	#creating widget objects
+	#creating objects
 	button = Button.new()
-	panel = Panel.new()
 	label = Label.new()
 	
+	#Setting their anchor to ratio!
+	#Helps with responsive design. 
+	button.set_anchor(0, ANCHOR_RATIO)
+	label.set_anchor(0, ANCHOR_RATIO)
+	
 	#Make a new post node and add it as a child of panel
-	var node = get_node("post").duplicate()
-	add_child(node)
+	#Post is the object which holds all the task names and buttons to check off task
+	var node = get_parent().get_node("post").duplicate()
+	get_parent().add_child(node)
 	
 	#set up button
 	button.set_text(text)
 	button.connect("pressed", node, "post_task")
-	
-
-	
-	
 	node.task_id = id
 	
 	#naming nodes
 	button.set_name("button" + id)
-	panel.set_name(id)
 	label.set_name("label" + id)
 	
-	
 	label.set_text(text)
-	node.add_child(panel)
-	panel.add_child(button)
-	panel.add_child(label)
+
+	node.add_child(button)
+	node.add_child(label)
 	
-	button.set_begin(button_size_begin)
-	button.set_end(button_size_end)
-	panel.set_end(panel_size_end)
-	panel.set_pos(panel_pos)
+	button.set_global_pos(Vector2(node.get_global_pos().x + 30, node.get_global_pos().y))
+	print("button" + str(button.get_global_pos()))
+	print(node.get_global_pos())
+	print(label.get_global_pos())
+	node.set_end(panel_size_end)
+	node.set_pos(panel_pos)
 
 
 func _ready():
-	print("HELLO O O O ")
+	pass
